4bdc995d54f648f1218340b24182167a69c851ee
Vimannyu <vimannyus@gmail.comgit config --global user.email vimannyus@gmail.comgit config --global user.email vimannyus@gmail.com>
2023-05-18 04:27:06 +0530

    added env var

--- src/server.js
+++ src/server.js
@@ -1,4 +1,64 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const express = require('express')
+
+// console.log('hello world');
 
 
-console.log('hello world');
+
+//------------------------------------------------------------
+
+
+/* eslint-disable node/no-unpublished-import */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+import transRoutes from "./routes/transRoutes.js";
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config({ path: "./config.env" });
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Vimannyu:JFzRIEwbqIhI4Y9B@clustervimwallet.5iycf.mongodb.net/walletDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`DB CONNECTION SUCCESSFUL`);
+  })
+  .catch((err) => console.error(err));
+
+const port = 8000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}!!`);
+});
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use("/api/v1/trans", transRoutes);
+app.use("/api/v1/user", userRoutes);
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  const status = error.statusCode || 500;
+  const { message } = error;
+  const { data } = error;
+  res.status(status).json({ message: message, data: data });
+});
+
+
